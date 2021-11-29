611cb1fcb7cbf37045bfe8f3db4a02091910cf9f
Kwame AMANFO <kamanfo@e3r12p22.clusters.42paris.fr>
2021-11-29 14:23:44 +0100

    printf

---
+++ libft/ft_putnbr.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+
+void	ft_putnbr(int n)
+{
+	long long int	nb;
+
+	nb = n;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar((nb % 10) + '0');
+	}
+	else
+		ft_putchar(nb + '0');
+}
